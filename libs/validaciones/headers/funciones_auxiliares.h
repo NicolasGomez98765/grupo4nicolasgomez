baabc2ebc21d1664fb2c04e87e808f81e9ab6927
walterzaq <83846224+walterzaq@users.noreply.github.com>
2025-04-03 18:13:03 -0300

    Delete libs/validaciones directory

--- libs/validaciones/headers/funciones_auxiliares.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCIONES_AUXILIARES_H_INCLUDED
-#define FUNCIONES_AUXILIARES_H_INCLUDED
-
-void imprimir_vector_int_terminaEnMenosUno(int vec[]);
-
-#endif // FUNCIONES_AUXILIARES_H_INCLUDED
